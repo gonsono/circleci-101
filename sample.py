4b4a1766ffdbc4dae86c6b7ea4d5f9d6de72bd10
Olivier Gonson <olivier.gonson@lightspeedhq.com>
2019-01-25 13:15:53 -0500

    add build script

--- sample.py
+++ sample.py
@@ -1,10 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+app.run(host= '0.0.0.0')
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
